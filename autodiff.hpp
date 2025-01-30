611574aa4eebcb4133a48efa69c0719849d614f8
Pietro Beghetto <pietro.beghetto@mail.polimi.it>
2025-01-30 20:33:21 +0000

    added relu and equals

--- autodiff.hpp
+++ autodiff.hpp
@@ -203,12 +203,9 @@ Var<T> operator+(T const & lhs, Var<T> const & rhs) {
     return operator+(rhs, lhs);
 }
 
-
-
 /*------------------------------------------------------------*/
 /* forward accumulation                                       */
 /*------------------------------------------------------------*/
-
 
 template <typename T>
 DualVar<T> operator+(DualVar<T> const & lhs, DualVar<T> const & rhs){
@@ -229,6 +226,7 @@ DualVar<T> operator+(T const & lhs, DualVar<T> const & rhs){
 /***************************************************************/
 /* SUB                                                         */
 /***************************************************************/
+
 template <typename T>
 Var<T> operator-(Var<T> const & lhs, Var<T> const & rhs) {
     auto & tape = Tape<T>::getTape();
@@ -244,11 +242,9 @@ Var<T> operator-(Var<T> const & lhs, Var<T> const & rhs) {
     return var;
 }
 
-
 /*------------------------------------------------------------*/
 /* forward accumulation                                       */
 /*------------------------------------------------------------*/
-
 
 template <typename T>
 DualVar<T> operator-(DualVar<T> const & lhs, DualVar<T> const & rhs){
@@ -268,6 +264,7 @@ DualVar<T> operator-(T const & lhs, DualVar<T> const & rhs){
 /***************************************************************/
 /* MUL                                                         */
 /***************************************************************/
+
 template <typename T>
 Var<T> operator*(Var<T> const & lhs, Var<T> const & rhs) {
     auto & tape = Tape<T>::getTape();
@@ -286,7 +283,6 @@ Var<T> operator*(Var<T> const & lhs, Var<T> const & rhs) {
 /*------------------------------------------------------------*/
 /* forward accumulation                                       */
 /*------------------------------------------------------------*/
-
 
 template <typename T>
 DualVar<T> operator*(DualVar<T> const & lhs, DualVar<T> const & rhs){
@@ -307,6 +303,7 @@ DualVar<T> operator*(T const & lhs, DualVar<T> const & rhs){
 /***************************************************************/
 /* DIV                                                         */
 /***************************************************************/
+
 template <typename T>
 Var<T> operator/(Var<T> const & lhs, Var<T> const & rhs) {
     auto & tape = Tape<T>::getTape();
@@ -326,7 +323,6 @@ Var<T> operator/(Var<T> const & lhs, Var<T> const & rhs) {
 /* forward accumulation                                       */
 /*------------------------------------------------------------*/
 
-
 template <typename T>
 DualVar<T> operator/(DualVar<T> const & lhs, DualVar<T> const & rhs){
     return DualVar<T>(lhs.getReal() / rhs.getReal(), 
@@ -343,7 +339,6 @@ DualVar<T> operator/(T const & lhs, DualVar<T> const & rhs){
     return DualVar<T> (lhs / rhs.getReal(), lhs * rhs.getInf() / (rhs.getReal() * rhs.getReal()));
 }
 
-
 /***************************************************************/
 /* MISC                                                        */
 /***************************************************************/
@@ -352,29 +347,29 @@ DualVar<T> operator/(T const & lhs, DualVar<T> const & rhs){
 /*-------------------------------------------------------------*/
 
 template <typename T>
-inline DualVar<T> abs(const DualVar<T> & arg){
+inline DualVar<T> abs(DualVar<T> const & arg){
     int sign_real = (arg.getReal() > 0) ? 1 : ((arg.getReal() < 0) ? -1 : 0);
     return DualVar<T> (std::abs(arg.getReal()), arg.getInf() * sign_real);
 }
 
 template <typename T>
-inline DualVar<T> cos(const DualVar<T> & arg){
+inline DualVar<T> cos(DualVar<T> const & arg){
     return DualVar<T> (std::cos(arg.getReal()), - arg.getInf() * std::sin(arg.getReal()));
 }
 
 template <typename T>
-inline DualVar<T> sin(const DualVar<T> & arg){
+inline DualVar<T> sin(DualVar<T> const & arg){
     return DualVar<T> (std::sin(arg.getReal()), arg.getInf() * std::cos(arg.getReal()));
 }
 
 template <typename T>
-inline DualVar<T> tan(const DualVar<T> & arg){
+inline DualVar<T> tan(DualVar<T> const & arg){
     return DualVar<T> (std::tan(arg.getReal()), 
         arg.getInf() / (std::cos(arg.getReal()) * std::cos(arg.getReal())));
 }
 
 template <typename T>
-inline DualVar<T> log(const DualVar<T> & arg){
+inline DualVar<T> log(DualVar<T> const & arg){
     return DualVar<T> (std::log(arg.getReal()), arg.getInf() / arg.getReal());
 }
 
@@ -382,24 +377,38 @@ inline DualVar<T> log(const DualVar<T> & arg){
 /* When raising a dual number to the power of another dual number, you get
    (a+b𝜀)^(c+d𝜀) = a^c + a^(c-1)*(a*d*ln(a) + c*b)𝜀                         */
 template <typename T>
-inline DualVar<T> pow(DualVar<T> & base, DualVar<T> & exp){
+inline DualVar<T> pow(DualVar<T> const & base, DualVar<T> const & exp){
     return DualVar<T> (std::pow(base.getReal(), exp.getReal()), std::pow(base.getReal(), exp.getReal() - 1) * 
         (base.getReal() * exp.getInf() * std::log(base.getReal()) + exp.getReal() * base.getInf()));
 }
 
 template <typename T>
-inline DualVar<T> pow(T const & base, DualVar<T> & exp){
+inline DualVar<T> pow(T const & base, DualVar<T> const & exp){
     return DualVar<T> (std::pow(base, exp.getReal()), 
         std::pow(base, exp.getReal()) * exp.getInf() * std::log(base));
 }
 
 template <typename T>
-inline DualVar<T> pow(DualVar<T> & base, T const & exp){
+inline DualVar<T> pow(DualVar<T> const & base, T const & exp){
     return DualVar<T> (std::pow(base.getReal(), exp), 
         std::pow(base.getReal(), exp - 1) * exp * base.getInf());
-    
 }
 
+
+template <typename T>
+bool operator==(DualVar<T> const & lhs, DualVar<T> const & rhs){
+    if (lhs.getReal() == rhs.getReal() and lhs.getInf() == rhs.getInf()){ return true; }
+    return false;
+}
+
+template <typename T>
+DualVar<T> relu(DualVar<T> const & arg){
+    if (arg.getReal() > 0){
+        return DualVar<T> (arg.getReal(), 1);
+    } else {
+        return DualVar<T> (0, 0);
+    }
+}
 
 }; // namespace autodiff
 
--- test.cpp
+++ test.cpp
@@ -6,6 +6,10 @@
 int main() {
     autodiff::DualVar a(4.0, 6.0);
     autodiff::DualVar b(6.0, 2.0);
+    autodiff::DualVar c(-2.0, -1.0);
+    autodiff::DualVar d(2.0, -1.0);
+    autodiff::DualVar e(-2.0, 1.0);
+    autodiff::DualVar f(4.0, 6.0);
 
     std::cout << (autodiff::log(a)).getValue() << std::endl;
     std::cout << (autodiff::pow(a, b)).getValue() << std::endl;
@@ -16,5 +20,16 @@ int main() {
     std::cout << autodiff::cos(b).getValue() << std::endl;
     std::cout << autodiff::tan(b).getValue() << std::endl;
 
+    std::cout << "ABS VALUE" << std::endl;
+    std::cout << autodiff::abs(c).getValue() << std::endl;
+    std::cout << autodiff::abs(d).getValue() << std::endl;
+    std::cout << autodiff::abs(d).getValue() << std::endl;
+
+    std::cout << "RELU, ==" << std::endl;
+    std::cout << autodiff::relu(c).getValue() << std::endl;
+    std::cout << autodiff::relu(d).getValue() << std::endl;
+    std::cout << autodiff::relu(e).getValue() << std::endl;
+    std::cout << (b == a) << std::endl;
+    std::cout << (a == f) << std::endl;
     return 0;
 }
