5f5b4f7fdb96235b96f7a1d481566619fc08f177
song zhaohui <a554381721@gmail.com>
2025-02-01 13:38:49 +0100

    small fix

--- src/reverse/test.cpp
+++ src/reverse/test.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 #include "autodiff.hpp"
 #include "utils.hpp"
+#include "neural.hpp"
+
+using namespace std;
 
 int main() {
 
@@ -12,12 +15,15 @@ int main() {
     autodiff::Var y = tape.var(2.0);
     autodiff::Var z = tape.var(2.0);
     autodiff::Var w = x/x+y*z;
+    autodiff::Var k = w;
 
     // std::cout << tape.size() << std::endl;
 
-    utils::saveGraphToFile(w, "output_graph.png");
+    utils::saveGraphToFile(k, "output_graph.png");
 
     w.backward();
+
+    Neuron<double> a(10);
 
     return 0;
 }
