dc49ba8616c695bbc5dd64aa1363c1d4779dc7fb
song zhaohui <a554381721@gmail.com>
2025-05-14 11:13:40 +0200

    add linear regression using forward

--- src/forward/LinearModel.cpp
+++ src/forward/LinearModel.cpp
@@ -1,14 +1,28 @@
 #include "LinearModel.h"
+#include <chrono>
+#include <iostream>
+
+#include "LinearModelParallel.h"
 
 int main() {
-    auto data = generate_data(100, 5.0, -1.0);  // true model: y = 2x - 1
+    auto data = generate_data(100, 5.0, -2.0);  // true model: y = 2x - 1
+    // LinearModel serial execution time
     LinearModel model(0.01, 200, 20);            // lr, epochs, batch_size
-    model.fit(data);
+    LinearModelParallel modelParallel(0.01, 1000, 20);
 
-    std::cout << "\nPredictions:\n";
-    for (double x = 0.0; x <= 1.0; x += 0.2) {
-        std::cout << "x = " << x << ", y_pred = " << model.predict(x) << std::endl;
-    }
+
+    auto t0 = std::chrono::high_resolution_clock::now();
+    model.fit(data);
+    auto t1 = std::chrono::high_resolution_clock::now();
+    auto duration1 = std::chrono::duration_cast<std::chrono::microseconds>(t1 - t0);
+    std::cout << "Serial training took: " << duration1.count() << " microseconds" << std::endl;
+
+    t0 = std::chrono::high_resolution_clock::now();
+    modelParallel.fit(data);
+    t1 = std::chrono::high_resolution_clock::now();
+    auto duration2 = std::chrono::duration_cast<std::chrono::microseconds>(t1 - t0);
+    std::cout << "Parallel training took: " << duration2.count() << " microseconds" << std::endl;
+    std::cout << "Speed Up is " << static_cast<double>(1.0 * duration1 / duration2) << std::endl;
 
     return 0;
 }
--- src/forward/LinearModel.h
+++ src/forward/LinearModel.h
@@ -1,16 +1,109 @@
-//
-// Created by sheldon on 5/12/25.
-//
-
 #ifndef LINEARMODEL_H
 #define LINEARMODEL_H
 
+#include "autodiff.hpp"
+#include <iostream>
+#include <vector>
+#include <random>
+#include <algorithm>
+#include <cmath>
+
+using namespace autodiff;
+
+inline std::vector<std::pair<double, double>> generate_data(const int N, const double true_w, const double true_b)
+{
+    std::default_random_engine rng(42);
+    std::normal_distribution<double> noise(0.0, 0.1);
+    std::vector<std::pair<double, double>> data;
+    for (int i = 0; i < N; i++)
+    {
+        //generate some random data to learn a mapping
+        double x = i * 0.1;
+        double y = true_w * x + true_b + noise(rng);
+        data.emplace_back(x, y);
+    }
+    return data;
+}
 
 
-class LinearModel {
+/* this classe is for an example of a simple regression*/
+class LinearModel{
+private:
+    double w;
+    double b;
+    double lr;
+    int epochs;
+    int batch_size;
+
+    DualVar<double> loss_func(const std::vector<std::pair<double, double>>& batch,
+                                DualVar<double> w_,
+                                DualVar<double> b_)
+    {
+        double real_accum = 0.0;
+        double inf_accum = 0.0;
+        for (const auto& [x, y] : batch)
+        {
+            DualVar x_dual(x, 0.0);
+            DualVar y_dual(y, 0.0);
+            DualVar<double> y_pred = w_ * x_dual + b_;
+            DualVar<double> diff = y_pred - y_dual;
+            //this diff * diff  is the loss squared, we want to know dloss/dw and dloss/db
+            //the derivative of this would be 2*loss * dloss/dw and 2*loss * dloss/db
+            real_accum += (diff * diff).getReal();
+            inf_accum += (diff * diff).getInf();
+        }
+        return DualVar<double>(real_accum / batch.size(), inf_accum / batch.size());
+    }
+
+    public:
+    LinearModel(double lr = 0.01, int epochs = 50, int batch_size = 10)
+        :w(0.0), b(0.0), lr(lr), epochs(epochs), batch_size(batch_size){}
+
+    void fit(std::vector<std::pair<double, double>>& data)
+    {
+        std::vector<double> params = {w, b};
+        std::default_random_engine rng(0);
+        for (int epoch = 0; epoch < epochs; epoch++)
+        {
+            std::shuffle(data.begin(), data.end(), rng);
+            //this part can be parallel
+            for (int i = 0; i < data.size(); i++)
+            {
+                std::vector<std::pair<double, double>> batch(
+                    data.begin() + i,
+                    data.begin() + std::min(i + batch_size, static_cast<int>(data.size())));
+                auto grad = gradient([&](const std::vector<DualVar<double>> &p)
+                {
+                    return loss_func(batch, p[0], p[1]);
+                }, params);
+                for (int j = 0; j < params.size(); j++)
+                    params[j] -= lr * grad[j];
+            }
+
+            auto loss_val = loss_func(data,
+                DualVar<double>(params[0], 0.0),
+                DualVar<double>(params[1], 0.0)).getReal();
+
+            /*std::cout << "Epoch " << epoch
+                << " | loss: " << loss_val
+                << " | w: " << params[0]
+                << " | b: " << params[1] << std::endl;*/
+        }
+        std::cout << " | w: " << params[0]
+                << " | b: " << params[1] << std::endl;
+        w = params[0];
+        b = params[1];
+    }
+
+    double predict(double x) const
+    {
+        return w * x + b;
+    }
+
+    std::pair<double, double> get_params() const{
+        return {w, b};
+    }
 
 };
-
-
 
 #endif //LINEARMODEL_H
