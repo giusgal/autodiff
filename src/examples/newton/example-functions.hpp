c436d7fe50f416a6ae05cc9303dc6d8717b64ba4
pietro-beghetto <pietro.beghetto@mail.polimi.it>
2025-06-12 12:12:41 +0200

    removed extra argument

--- src/examples/newton/example-functions.hpp
+++ src/examples/newton/example-functions.hpp
@@ -18,7 +18,7 @@ namespace testfun {
 
     CUDA_DEVICE dv cu_f0(const dvec &x) { return x[0] * 1.0 + x[1] * 2.0 + x[2] * 4.0; };
     newton::CudaFunctionWrapper<double> createcudafn() {
-        newton::CudaFunctionWrapper<double> cudafun(2);
+        newton::CudaFunctionWrapper<double> cudafun();
         cudafun.add_output<cu_f0>();
         return cudafun;
     }
